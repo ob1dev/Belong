31412f9d552d85e0c76e56c2cf2ad3b66bbf040a
Oleg Burov <oleg.burov@outlook.com>
2018-04-21 13:34:09 -0700

    Add account model that describes a user.

---
+++ src/WebApp/Models/AccountModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace WebApp.Models
+{
+  public class AccountModel
+  {
+    public Guid UserId { get; set; }
+
+    [Required, MaxLength(255)]
+    public string FirstName { get; set; }
+
+    [Required, MaxLength(255)]
+    public string LastName { get; set; }
+
+    [Required, EmailAddress]
+    public string Email { get; set; }
+
+    [Required, Phone]
+    public string Phone { get; set; }
+
+    public IPAddress IpAddress { get; set; }
+  }
+}
