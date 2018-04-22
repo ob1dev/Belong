7599eb02515224055b4b38a49903c565df24bbd1
Oleg Burov <oleg.burov@outlook.com>
2018-04-21 20:59:01 -0700

    Add class AppDbContext.
    Add class AppDbContext to store Accounts.

---
+++ src/WebApp/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+  public class AppDbContext : DbContext
+  {
+    public DbSet<AccountModel> Accounts { get; set; }
+
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+  }
+}
