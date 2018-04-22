32e1037949cd2d0e6cb3884dfa0a0845ca9a221d
Oleg Burov <oleg.burov@outlook.com>
2018-04-21 21:20:12 -0700

    Made code cleanup and format.

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -1,53 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using WebApp.Data;
 using WebApp.Models;
 
 namespace WebApp.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private AppDbContext dbContext;
+
+    public HomeController(AppDbContext context)
     {
-        private AppDbContext dbContext;
-
-        public HomeController(AppDbContext context)
-        {
-            this.dbContext = context;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Terms()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Join(AccountModel account)
-        {
-            account.IpAddress = HttpContext.Connection.RemoteIpAddress.GetAddressBytes();
-
-            this.dbContext.Accounts.Add(account);
-            await this.dbContext.SaveChangesAsync();
-
-            return RedirectToAction("Index");
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      this.dbContext = context;
     }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Terms()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Join(AccountModel account)
+    {
+      account.IpAddress = HttpContext.Connection.RemoteIpAddress.GetAddressBytes();
+
+      this.dbContext.Accounts.Add(account);
+      await this.dbContext.SaveChangesAsync();
+
+      return RedirectToAction("Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
