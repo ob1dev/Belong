a31433c7169c950136a02324df059dce28639772
Oleg Burov <oleg.burov@outlook.com>
2018-04-22 15:01:46 -0700

    Make field 'AddressId' nullable.
    The field 'AddressId' can be null when creating an account.

---
+++ src/WebApp/Data/Migrations/20180422215807_AddressIdIsNullable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class AddressIdIsNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Addresses_AddressId",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AddressId",
+                table: "Accounts",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Addresses_AddressId",
+                table: "Accounts",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Addresses_AddressId",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AddressId",
+                table: "Accounts",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Addresses_AddressId",
+                table: "Accounts",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
