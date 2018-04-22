0814fa05bc107100d4fb032b66656559dcedebf3
Oleg Burov <oleg.burov@outlook.com>
2018-04-21 21:16:13 -0700

    Add DbContext and acction for creating an account.

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -4,12 +4,20 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
 using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private AppDbContext dbContext;
+
+        public HomeController(AppDbContext context)
+        {
+            this.dbContext = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -23,6 +31,17 @@ namespace WebApp.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Join(AccountModel account)
+        {
+            account.IpAddress = HttpContext.Connection.RemoteIpAddress.GetAddressBytes();
+
+            this.dbContext.Accounts.Add(account);
+            await this.dbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
