73fc92964c87926cbd0c3089d3bd531e3486a67c
Oleg Burov <oleg.burov@outlook.com>
2018-04-21 20:58:09 -0700

    Add DbContext service.
    1. Add DbContext service using SqlServer.
    2. Add connection string for a local SqlServer.

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApp.Data;
 
 namespace WebApp
 {
@@ -24,6 +26,9 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
