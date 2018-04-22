f09a9e5a99e1510ffd64a6e256181308d7cb4e35
Oleg Burov <oleg.burov@outlook.com>
2018-04-22 11:40:37 -0700

    Add EF Core AddressModel migration.

---
+++ src/WebApp/Data/Migrations/20180422183742_AddressModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class AddressModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    StreetNumber = table.Column<string>(maxLength: 10, nullable: false),
+                    StreetName = table.Column<string>(maxLength: 255, nullable: false),
+                    City = table.Column<string>(maxLength: 100, nullable: false),
+                    State = table.Column<string>(maxLength: 2, nullable: false),
+                    PostalCode = table.Column<string>(maxLength: 5, nullable: false),
+                    Country = table.Column<string>(maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
