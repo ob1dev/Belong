367800b0f3ce08ed32a7867c5d71337d0f72b026
Oleg Burov <oleg.burov@outlook.com>
2018-04-22 16:50:59 -0700

    Add dedicated controller to handle account sign in.

---
+++ src/WebApp/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+  [Route("[controller]/[action]")]
+  public class AccountController : Controller
+  {
+    private AppDbContext dbContext;
+
+    public AccountController(AppDbContext context)
+    {
+      this.dbContext = context;
+    }
+  }
+}
