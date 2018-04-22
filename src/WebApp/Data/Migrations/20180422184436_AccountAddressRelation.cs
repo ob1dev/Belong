a04dd1c77856a1b392e7952303e8b7e56dd1a8b1
Oleg Burov <oleg.burov@outlook.com>
2018-04-22 11:46:56 -0700

    Add EF Core migration for relation between Account and Address.

---
+++ src/WebApp/Data/Migrations/20180422184436_AccountAddressRelation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class AccountAddressRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AddressId",
+                table: "Accounts",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_AddressId",
+                table: "Accounts",
+                column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Addresses_AddressId",
+                table: "Accounts",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Addresses_AddressId",
+                table: "Accounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_AddressId",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Accounts");
+        }
+    }
+}
