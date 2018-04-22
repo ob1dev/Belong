ea8b0f9929b70e6a7c403a10dd80bec5e14e46cf
Oleg Burov <oleg.burov@outlook.com>
2018-04-22 16:59:27 -0700

    Bring back default controller and views.
    Bring back default controller and views to link them with Signin form.

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -1,22 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Threading.Tasks;
-using WebApp.Data;
 using WebApp.Models;
 
 namespace WebApp.Controllers
 {
   public class HomeController : Controller
   {
-    private AppDbContext dbContext;
-
-    public HomeController(AppDbContext context)
-    {
-      this.dbContext = context;
-    }
-
     public IActionResult Index()
     {
+      return View();
+    }
+
+    public IActionResult About()
+    {
+      ViewData["Message"] = "Your application description page.";
+
+      return View();
+    }
+
+    public IActionResult Contact()
+    {
+      ViewData["Message"] = "Your contact page.";
+
       return View();
     }
 
@@ -28,17 +33,6 @@ namespace WebApp.Controllers
     public IActionResult Privacy()
     {
       return View();
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Join(AccountModel account)
-    {
-      account.IpAddress = HttpContext.Connection.RemoteIpAddress.GetAddressBytes();
-
-      this.dbContext.Accounts.Add(account);
-      await this.dbContext.SaveChangesAsync();
-
-      return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
