da8b9170deb2718c0cdad91066e3b6ad1a9f5549
Oleg Burov <oleg.burov@outlook.com>
2018-04-22 17:02:45 -0700

    Fix spacing.

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -26,8 +26,8 @@ namespace WebApp
 
       services.Configure<CookiePolicyOptions>(options =>
       {
-              // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-              options.CheckConsentNeeded = context => true;
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = context => true;
         options.MinimumSameSitePolicy = SameSiteMode.None;
       });
 
