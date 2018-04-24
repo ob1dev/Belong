c60dfe22976137b4b9795e7a79465f5214280c8b
Oleg Burov <oleg.burov@outlook.com>
2018-04-24 15:59:39 -0700

    Add EF Core migration for field 'GooglePalceId' and its index.

--- src/WebApp/Data/Migrations/20180424220511_AddressModelAddGooglePlaceId.Designer.cs
+++ src/WebApp/Data/Migrations/20180424220511_AddressModelAddGooglePlaceId.Designer.cs
@@ -10,9 +10,10 @@ using WebApp.Data;
 namespace WebApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180424220511_AddressModelAddGooglePlaceId")]
+    partial class AddressModelAddGooglePlaceId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/WebApp/Data/Migrations/20180424220511_AddressModelAddGooglePlaceId.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class AddressModelAddGooglePlaceId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GooglePlaceId",
+                table: "Addresses",
+                maxLength: 27,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_GooglePlaceId",
+                table: "Addresses",
+                column: "GooglePlaceId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_GooglePlaceId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "GooglePlaceId",
+                table: "Addresses");
+        }
+    }
+}
