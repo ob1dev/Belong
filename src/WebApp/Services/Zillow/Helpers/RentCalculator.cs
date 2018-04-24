2e75f37dbaee6d462976bed15aa5ba6fe111af1a
Oleg Burov <oleg.burov@outlook.com>
2018-04-23 21:15:19 -0700

    Implement a simple rent calculator.

---
+++ src/WebApp/Services/Zillow/Helpers/RentCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WebApp.Services.Zillow.SearchResults;
+
+namespace WebApp.Services.Zillow.Helpers
+{
+  public class RentCalculator
+  {
+    private readonly int Rate = 5;
+    private readonly int Months = 12;
+    private readonly int Range = 10;
+
+    public ValuationRange GetRentBasedOnHomePrice(decimal homePrice)
+    {
+      var estimateRent = (homePrice * Rate / 100) / Months;
+      var rangeValue = estimateRent * Range / 100;
+      var low = Math.Truncate(estimateRent - rangeValue);
+      var high = Math.Truncate(estimateRent + rangeValue);
+
+      return new ValuationRange(low, high);
+    }
+  }
+}
