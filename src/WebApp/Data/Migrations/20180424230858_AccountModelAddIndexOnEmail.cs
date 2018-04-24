ffff884059a267c13113034b530a0fc315bc7139
Oleg Burov <oleg.burov@outlook.com>
2018-04-24 16:11:27 -0700

    Add EF Core migration for field 'Email' and its index.

---
+++ src/WebApp/Data/Migrations/20180424230858_AccountModelAddIndexOnEmail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class AccountModelAddIndexOnEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Accounts",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_Email",
+                table: "Accounts",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_Email",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Accounts",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
