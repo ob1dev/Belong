72f6d70db0fb1aadcf7c6630bea477b4584d3c16
Oleg Burov <oleg.burov@outlook.com>
2018-04-23 17:46:18 -0700

    Add EF Core RentEstimate migration.

--- src/WebApp/Data/Migrations/20180424004455_RentEstimateModel.Designer.cs
+++ src/WebApp/Data/Migrations/20180424004455_RentEstimateModel.Designer.cs
@@ -10,9 +10,10 @@ using WebApp.Data;
 namespace WebApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180424004455_RentEstimateModel")]
+    partial class RentEstimateModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/WebApp/Data/Migrations/20180424004455_RentEstimateModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class RentEstimateModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Addresses_AddressId",
+                table: "Accounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_AddressId",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Accounts");
+
+            migrationBuilder.CreateTable(
+                name: "RentEstimates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    RentZestimateLow = table.Column<decimal>(nullable: true),
+                    RentZestimateHigh = table.Column<decimal>(nullable: true),
+                    ExpectedRent = table.Column<decimal>(nullable: false),
+                    AccountId = table.Column<Guid>(nullable: false),
+                    AddressId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RentEstimates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RentEstimates_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RentEstimates_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentEstimates_AccountId",
+                table: "RentEstimates",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentEstimates_AddressId",
+                table: "RentEstimates",
+                column: "AddressId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RentEstimates");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AddressId",
+                table: "Accounts",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_AddressId",
+                table: "Accounts",
+                column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Addresses_AddressId",
+                table: "Accounts",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
