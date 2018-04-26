e8821045c0724e4eb81b42cb4cd5f7de2e4c62d8
Oleg Burov <oleg.burov@outlook.com>
2018-04-25 22:15:56 -0700

    Update EF Core migration for Phone field.

---
+++ src/WebApp/Data/Migrations/20180426051323_AccountModelAddPhoneLength.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class AccountModelAddPhoneLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Accounts",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Accounts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 10);
+        }
+    }
+}
